75e35910dc6508c56b80395c6b0c2f0df48438aa
Quentin <quentin.robard@beecoming.fr>
2022-03-16 13:43:53 +0100

    intial commit

---
+++ Assets/Scripts/HexCell.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+	[SerializeField]
+	HexCell[] neighbors;
+
+	public int elevation;
+
+	public HexCoordinates coordinates;
+	public Color color;
+
+	public HexCell GetNeighbor(HexDirection direction)
+	{
+		return neighbors[(int)direction];
+	}
+
+	public void SetNeighbor(HexDirection direction, HexCell cell)
+	{
+		neighbors[(int)direction] = cell;
+		cell.neighbors[(int)direction.Opposite()] = this;
+	}
+}
