f8e505db2d65d6cd7c529be3ad33dee64c276d1a
Quentin <quentin.robard@beecoming.fr>
2022-05-03 21:50:48 +0200

    7.1 - Cell with roads

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -69,6 +69,14 @@ public class HexCell : MonoBehaviour
 				RemoveIncomingRiver();
 			}
 
+			for (int i = 0; i < roads.Length; i++)
+			{
+				if (roads[i] && GetElevationDifference((HexDirection)i) > 1)
+				{
+					SetRoad(i, false);
+				}
+			}
+
 			Refresh();
 		}
 	}
@@ -206,15 +214,65 @@ public class HexCell : MonoBehaviour
 
 		hasOutgoingRiver = true;
 		outgoingRiver = direction;
-		RefreshSelfOnly();
 		
 		neighbor.RemoveIncomingRiver();
 		neighbor.hasIncomingRiver = true;
 		neighbor.incomingRiver = direction.Opposite();
-		neighbor.RefreshSelfOnly();
+
+		SetRoad((int)direction, false);
 	}
 	#endregion
 
+	#region roads
+	[SerializeField]
+	bool[] roads;
+	public bool HasRoads
+	{
+		get
+		{
+			for (int i = 0; i < roads.Length; i++)
+			{
+				if (roads[i])
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+	public bool HasRoadThroughEdge(HexDirection direction)
+	{
+		return roads[(int)direction];
+	}
+	public void AddRoad(HexDirection direction)
+	{
+		if (
+			!roads[(int)direction] && !HasRiverThroughEdge(direction) &&
+			GetElevationDifference(direction) <= 1
+		)
+		{
+			SetRoad((int)direction, true);
+		}
+	}
+	public void RemoveRoads()
+	{
+		for (int i = 0; i < neighbors.Length; i++)
+		{
+			if (roads[i])
+			{
+				SetRoad(i, false);
+			}
+		}
+	}
+	void SetRoad(int index, bool state)
+	{
+		roads[index] = state;
+		neighbors[index].roads[(int)((HexDirection)index).Opposite()] = state;
+		neighbors[index].RefreshSelfOnly();
+		RefreshSelfOnly();
+	}
+	#endregion
 
 	void Refresh()
 	{
@@ -260,5 +318,11 @@ public class HexCell : MonoBehaviour
 		return HexMetrics.GetEdgeType(
 			elevation, otherCell.elevation
 		);
+	}
+
+	public int GetElevationDifference(HexDirection direction)
+	{
+		int difference = elevation - GetNeighbor(direction).elevation;
+		return difference >= 0 ? difference : -difference;
 	}
 }
