9e5e2f08dc75978b7cc374298cd02713a427ee9c
Quentin <quentin.robard@beecoming.fr>
2022-05-14 18:28:31 +0200

    add colors depending to environment

--- Assets/Scripts/GenProc/Biomes/City.cs
+++ Assets/Scripts/GenProc/Biomes/City.cs
@@ -14,7 +14,7 @@ public class City : Biome
         FlattenCells(grid.GetCells(), 0.8);
 
         //On place les rivières
-        int nbRivers = rand.Next(3,5);
+        int nbRivers = rand.Next(3, 5);
         List<HexCell> riversEnd = new List<HexCell>();
         for (int i = 0; i < nbRivers; i++)
         {
@@ -23,9 +23,9 @@ public class City : Biome
         }
 
         //On ajoutes des lacs (si possible) au bout des rivières
-        foreach(HexCell cell in riversEnd)
+        foreach (HexCell cell in riversEnd)
         {
-            if(cell.WaterLevel == 0)
+            if (cell.WaterLevel == 0)
             {
                 //Ajout de lacs
                 List<HexCell> waterCells = new List<HexCell>();
@@ -33,7 +33,7 @@ public class City : Biome
                 GenerateWater(cell, waterCells, waterLevel);
 
                 //Si on remplis pas trop la carte d'eau
-                if(((double)waterCells.Count / (double)grid.GetCells().Length) < 0.5)
+                if (((double)waterCells.Count / (double)grid.GetCells().Length) < 0.5)
                 {
                     //On ajoutes l'eau aux tuiles
                     foreach (HexCell waterCell in waterCells)
@@ -42,18 +42,24 @@ public class City : Biome
                     }
                 }
             }
-            
+
         }
 
         //Créer plusieurs villes en fonction de la taille de la carte (nombre de cell)
         int nbCities = 1 + (grid.GetCells().Length / 150);
-        for(int i = 0; i<nbCities; i++)
+        for (int i = 0; i < nbCities; i++)
         {
-            int minLength = rand.Next(Math.Min(grid.chunkCountX , grid.chunkCountZ), Math.Max(grid.chunkCountX , grid.chunkCountZ));
-            int maxLength = rand.Next(         grid.chunkCountX + grid.chunkCountZ ,          grid.chunkCountX * grid.chunkCountZ );
+            int minLength = rand.Next(Math.Min(grid.chunkCountX, grid.chunkCountZ), Math.Max(grid.chunkCountX, grid.chunkCountZ));
+            int maxLength = rand.Next(grid.chunkCountX + grid.chunkCountZ, grid.chunkCountX * grid.chunkCountZ);
 
             int citySize = rand.Next(minLength, maxLength);//todo: random map size
             GenerateCity(citySize);
+        }
+
+        //Mettre de la couleur sur les cases
+        foreach (HexCell cell in grid.GetCells())
+        {
+            ChangeColor(cell);
         }
     }
 
@@ -72,8 +78,8 @@ public class City : Biome
         HexCell from = highestCellAvailable;
 
         //taille de la rivière
-        int minLength = rand.Next(Math.Min(grid.chunkCountX , grid.chunkCountZ), Math.Max(grid.chunkCountX , grid.chunkCountZ));
-        int maxLength = rand.Next(         grid.chunkCountX + grid.chunkCountZ ,          grid.chunkCountX * grid.chunkCountZ );
+        int minLength = rand.Next(Math.Min(grid.chunkCountX, grid.chunkCountZ), Math.Max(grid.chunkCountX, grid.chunkCountZ));
+        int maxLength = rand.Next(grid.chunkCountX + grid.chunkCountZ, grid.chunkCountX * grid.chunkCountZ);
 
         int riverLength = rand.Next(minLength, maxLength); //todo: faire varier en fonction de la taille de la map
 
@@ -87,10 +93,10 @@ public class City : Biome
     private void GenerateWater(HexCell currentCell, List<HexCell> cells, int waterLevel)
     {
         cells.Add(currentCell);
-        foreach(HexDirection direction in Enum.GetValues(typeof(HexDirection)))
+        foreach (HexDirection direction in Enum.GetValues(typeof(HexDirection)))
         {
             HexCell neighbor = currentCell.GetNeighbor(direction);
-            if(neighbor != null)
+            if (neighbor != null)
             {
                 if (neighbor.Elevation < waterLevel && !cells.Contains(neighbor))
                 {
@@ -103,7 +109,8 @@ public class City : Biome
     private void GenerateCity(int citySize)
     {
         HexCell cityCenter;
-        do {
+        do
+        {
             //On détermine le centre de la ville
             int randX = rand.Next(grid.chunkCountX * HexMetrics.chunkSizeX);
             int randZ = rand.Next(grid.chunkCountX * HexMetrics.chunkSizeZ / 2);
@@ -118,6 +125,32 @@ public class City : Biome
             HexDirection randDir = (HexDirection)directions.GetValue(rand.Next(directions.Length));
             int roadLength = rand.Next(0, citySize);
             GenerateRoad(cityCenter, randDir, roadLength, 0.5);
+        }
+    }
+
+    private void ChangeColor(HexCell cell)
+    {
+        if (cell.Elevation < cell.WaterLevel) { // SEA
+            //Si la profondeur est élevée
+            if ((cell.WaterLevel - cell.Elevation) > 1)
+                cell.Color = Colors.DEEP_SEA;
+            else
+                cell.Color = Colors.SEA;
+        }
+        else if (cell.HaveFloodedNeighbor()) { // SAND
+            cell.Color = Colors.SAND;
+        }
+        else if (cell.HasRoads) { // ROAD
+            cell.Color = Colors.PAVED_ROAD;
+        }
+        else if (cell.Elevation > 5) { // SNOW
+            cell.Color = Colors.SNOW;
+        }
+        else if (cell.Elevation > 3) { // MOUNTAIN
+            cell.Color = Colors.MOUNTAIN;
+        }
+        else { // FOREST
+            cell.Color = Colors.FOREST;
         }
     }
 }
---
+++ Assets/Scripts/GenProc/Biomes/Datas/Colors.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class Colors
+{
+    public static Color DEEP_SEA = new Color(0.05f, 0.24f, 0.39f);
+    public static Color SEA      = new Color(0.00f, 0.66f, 0.74f);
+    public static Color SAND     = new Color(0.81f, 0.71f, 0.62f);
+    
+    public static Color PAVED_ROAD  = new Color(0.57f, 0.56f, 0.58f);
+    public static Color TARRED_ROAD = new Color(0.24f, 0.24f, 0.25f);
+    public static Color DIRT_ROAD   = new Color(0.63f, 0.44f, 0.34f);
+
+    public static Color FIELD  = new Color(0.96f, 0.88f, 0.58f);
+    public static Color FOREST = new Color(0.28f, 0.42f, 0.20f);
+
+    public static Color MOUNTAIN = new Color(0.26f, 0.60f, 0.28f);
+    public static Color SNOW     = new Color(0.88f, 0.89f, 0.87f);
+}
